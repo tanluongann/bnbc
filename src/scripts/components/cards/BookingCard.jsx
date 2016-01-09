d7dcf86fec4411fb45a111c5cf3da97d7cae4bb8
Jeremy Tan <tanluongann@gmail.com>
2016-01-09 13:06:04 +0000

    IMprove layout

--- src/scripts/components/cards/BookingCard.jsx
+++ src/scripts/components/cards/BookingCard.jsx
@@ -21,12 +21,15 @@ var BookingCard = React.createClass({
     },
 
     render: function() {
+        
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        
         var bd = new Date(this.props.booking.date * 1000);
         var classes = ['card', 'booking'];
         if (!this.state.mounted) classes.push('mounting');
         
         var ist = {
-            "background": "url('/static/img/places/" + this.props.booking.place.photo + ".jpg')",
+            "background": "url('/static/img/places/" + this.props.booking.place.photo + ".png')",
         }
         
         return <article className={ classes.join(" ") } >
@@ -36,7 +39,7 @@ var BookingCard = React.createClass({
             <section>
                 <span className="date">
                     <span className="day">{ bd.getDate() }</span>
-                    <span className="month">{ bd.getMonth() }</span>
+                    <span className="month">{ months[bd.getMonth() - 1] }</span>
                 </span>
                 <span className="location">{ this.props.booking.place.name }</span>
                 <span className="duration">{ this.props.booking.duration } days</span>
