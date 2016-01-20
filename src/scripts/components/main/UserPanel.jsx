de35864271aa636b70264b0eb0506a9580af2c1e
Jeremy Tan <tanluongann@gmail.com>
2016-01-20 12:38:24 +0000

    Setup the side menu

--- src/scripts/components/main/UserPanel.jsx
+++ src/scripts/components/main/UserPanel.jsx
@@ -24,7 +24,19 @@ var UserPanel = React.createClass({
         var classes = ['userpanel'];
         if (!this.state.mounted) classes.push('mounting');
         return <div className={ classes.join(" ") } >
-            { this.props.user.name }
+            <span className="name">{ this.props.user.name }</span>
+            <img className="profile-picture" href="/static/img/{ this.props.user.picture }" />
+            <span className="badges">{ this.props.user.name }</span>
+            <span className="infos">
+                <span className="traveltime">
+                    <span className="label">Travel time: </span>
+                    <span className="value">589 days</span>
+                </span>
+                <span className="traveldistance">
+                    <span className="label">Travel distance: </span>
+                    <span className="value">65456 km</span>
+                </span>
+            </span>
         </div>
     },
 
